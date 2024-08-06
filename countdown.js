acf72d00c537c359aa2e66a45c529c6a5baca4db
walachu <gustavopimentel0406@gmail.com>
2024-08-05 22:03:29 -0500

    initial commit

---
+++ countdown.js
@@ -0,0 +1,24 @@
+// countdown.js
+
+function countdown() {
+    const eventDate = new Date("August 11, 2024 00:00:00").getTime();
+    const now = new Date().getTime();
+    const distance = eventDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+
+    if (distance < 0) {
+        clearInterval(countdownInterval);
+        document.getElementById("countdown").innerHTML = "¡El evento ha comenzado!";
+    }
+}
+
+const countdownInterval = setInterval(countdown, 1000);
